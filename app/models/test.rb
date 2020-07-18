a8ae471fd26fe33a12196438f5bd58ca25d21adc
Alex Govorov <alex@govorov.pro>
2020-07-18 19:41:18 +0300

    Rails Model в действии. Часть 1: интерфейс запросов ActiveRecord

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,11 @@
 class Test < ApplicationRecord
+  has_many :user_test
+  has_many :users, through: :user_test
+
+  def self.by_category_title(category_title)
+    # Тут скорее более красиво можно сделать через ассоциации, но в учебных целях
+    # решил освоить joins через SQL запрос
+    sql = 'INNER JOIN categories ON tests.category_id = categories.id'
+    joins(sql).where(categories: { title: category_title }).order(title: :desc).pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :user_test
+  has_many :tests, through: :user_test
+
+  def tests_by_level(test_level)
+    tests.where(level: test_level)
+  end
 end
