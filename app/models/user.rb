69d35d0da36b8ba6442247072a1ad9f8d21f83ed
Alex Govorov <alex@govorov.pro>
2020-07-31 14:10:26 +0300

    User model syntax fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   has_many :tests, through: :user_test, dependent: :destroy
   has_many :created_tests, class_name: 'Test', dependent: :destroy
 
+  validates :email, presence: true
+
   def tests_by_level(test_level)
     tests.by_level(test_level)
   end
-
-  validates :email, presence: true
 end
