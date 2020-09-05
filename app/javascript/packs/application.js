4591234a768ebaacf7c4aed3aed848e01f422723
Alex Govorov <alex@govorov.pro>
2020-09-05 23:21:40 +0300

    JS fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,10 +9,13 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require("packs/utilities/sorting")
+require("utilities/form_inline")
+require("utilities/progress_bar")
+require("utilities/pwd_confirmation")
+require("utilities/sorting")
 
-window.jQuery = $
-window.$ = $
+// window.jQuery = $
+// window.$ = $
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
