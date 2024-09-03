21115723f1b835cf0a680a9e799b4ee1fb9cf677
Alex-Govorov <alex@govorov.pro>
2024-09-03 19:32:12 +0300

    feat: seeds.rb generates initial DB data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_29_094401) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_03_143607) do
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.boolean "correct", default: false, null: false
@@ -41,6 +41,13 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_29_094401) do
     t.datetime "updated_at", null: false
     t.integer "category_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+  end
+
+  create_table "tests_users", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "test_id"
+    t.index ["test_id"], name: "index_tests_users_on_test_id"
+    t.index ["user_id"], name: "index_tests_users_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,32 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+USERS = %w[Alex Eve V]
+CATEGORIES = %w[Ruby Go Elixir]
+TESTS = %w[Operators Conditions Iterators]
+
+USERS.each { |user_name| User.create(name: user_name) }
+
+CATEGORIES.each { |category| Category.create(title: category) }
+
+20.times { Test.create(title: TESTS.sample,
+                       level: rand(9),
+                       category_id: Category.all.to_a.sample.id) }
+
+20.times do
+  Question.create(body: 'What is one of the key benefits of using eager loading in Active Record?',
+                  test_id: Test.all.to_a.sample.id)
+
+  Answer.create(body: 'It increases the number of database queries',
+                question_id: Question.last.id)
+  Answer.create(body: 'It reduces the number of database queries needed for data retrieval',
+                question_id: Question.last.id,
+                correct: true)
+  Answer.create(body: 'It simplifies the database schema',
+                question_id: Question.last.id)
+  Answer.create(body: 'It automatically indexes all database fields',
+                question_id: Question.last.id)
+end
+
+20.times { User.all.to_a.sample.tests << Test.all.to_a.sample }
