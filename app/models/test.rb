b49a7aac66d261ad16d409ccf09b3b1710606dd3
Alex-Govorov <alex@govorov.pro>
2024-09-03 20:20:13 +0300

    feat: tests by category title

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,8 @@
 class Test < ApplicationRecord
   has_and_belongs_to_many :users
+
+  def self.by_category_title(title)
+    self.joins("INNER JOIN categories ON categories.id = tests.category_id")
+    .where(categories: { title: title }).order(title: :desc).pluck(:title)
+  end
 end
