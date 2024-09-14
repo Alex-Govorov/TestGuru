7931baca665310e820d24b781aa07554b343fef2
Alex-Govorov <alex@govorov.pro>
2024-09-14 23:29:17 +0300

    feat: Added associations beetwen tests and authors(users)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :tests
+  has_many :tests_published, class_name: "Test", foreign_key: "author_id"
 
   def tests_by_level(level)
     self.tests.where(level: level)
