f2eb0ebcc3bfeac4cdaf2d1bb5c6a71d27aeb91f
Alex-Govorov <alex@govorov.pro>
2024-09-17 09:45:25 +0300

    feat: validation - one question cah have from 1 to 4 answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,4 +4,12 @@ class Answer < ApplicationRecord
   scope :correct, -> { where(correct: true) }
 
   validates :body, presence: true
+  validate :one_question_answers_count
+
+  private
+
+  def one_question_answers_count
+    message = "Answers quantity for this question can be between 1 and 4"
+    errors.add(:base, message) if question.answers.size >= 4
+  end
 end
