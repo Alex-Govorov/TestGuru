09c6cbd361f78bb9eb5bc9c11627a14671591c7c
Alex-Govorov <alex@govorov.pro>
2024-09-17 03:19:01 +0300

    feat: scopes for tests by level

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,6 +4,10 @@ class Test < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_and_belongs_to_many :users, dependent: :destroy
 
+  scope :level_easy, -> {  where(level: 0..1) }
+  scope :level_normal, -> {  where(level: 2..4) }
+  scope :level_hard, -> {  where("level >= ?", 5) }
+
   def self.by_category_title(title)
     self.joins("INNER JOIN categories ON categories.id = tests.category_id")
     .where(categories: { title: title }).order(title: :desc).pluck(:title)
