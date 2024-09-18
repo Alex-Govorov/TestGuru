a9136f1c3d1d87d14947655ff7a2f009358f0568
Alex-Govorov <alex@govorov.pro>
2024-09-18 18:49:56 +0300

    feat: question not found exception

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,8 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: %i[index new create]
 
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
   def index
     @questions = @test.questions
   end
@@ -27,5 +29,9 @@ class QuestionsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def rescue_with_question_not_found
+    render plain: "Question was not found"
   end
 end
