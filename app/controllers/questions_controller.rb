c8923e4b7f7d05816bf0670f6d7b51c7c0178f31
Alex-Govorov <alex@govorov.pro>
2024-09-18 08:54:16 +0300

    feat: test question view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,9 @@ class QuestionsController < ApplicationController
     @questions = Test.find(params[:test_id]).questions
     render json: { questions: @questions }
   end
+
+  def show
+    @question = Question.find(params[:id])
+    render plain: @question.body
+  end
 end
