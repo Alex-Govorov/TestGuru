e248c4965091adf29c3970363a49d2b495b032d3
Alex-Govorov <alex@govorov.pro>
2024-09-18 18:30:44 +0300

    feat: create question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: %i[index]
+  before_action :find_test, only: %i[index new create]
 
   def index
     @questions = @test.questions
@@ -12,6 +12,12 @@ class QuestionsController < ApplicationController
   end
 
   def new
+  end
+
+  def create
+    question = @test.questions.create(params.require(:question).permit(:body))
+
+    render plain: question.inspect
   end
 
   private
