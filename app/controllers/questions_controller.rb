102fa5e1bf578a1430a05537c1726fe5a7238dae
Alex-Govorov <alex@govorov.pro>
2024-09-18 10:32:16 +0300

    feat: find_test callback

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,22 @@
 class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[index]
+
   def index
-    @questions = Test.find(params[:test_id]).questions
+    @questions = @test.questions
     render json: { questions: @questions }
   end
 
   def show
     @question = Question.find(params[:id])
     render plain: @question.body
+  end
+
+  def new
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
   end
 end
